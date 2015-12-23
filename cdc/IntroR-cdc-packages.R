9bc2d1bea6bda182220728b944408ac9e35b2f2f
Jenna Krall <jenna.krall@gmail.com>
2015-12-22 20:53:29 -0500

    initial add of cdc files

---
+++ cdc/IntroR-cdc-packages.R
@@ -0,0 +1,39 @@
+####
+# Instructions for Introduction to R course
+
+###
+# Course assumes user has installed:
+# R (version 3.1.2 or later)
+# RStudio
+
+###
+# All course files can be downloaded at http://www.jennakrall.com/cdc
+
+
+#### 
+# Packages necessary for Introduction to R at CDC course
+
+# Each package (plus its dependencies) are necessary
+# They can be installed using the following code from R (preferred)
+
+# Or from CRAN https://cran.r-project.org/web/packages/available_packages_by_name.html
+# In this case, R must be configured to look for the packages in the correct place
+
+
+install.packages("dplyr")
+install.packages("XLConnect")
+install.packages("Hmisc")
+install.packages("epitools")
+install.packages("contrast")
+install.packages("survival")
+install.packages("lme4")
+install.packages("ggplot2")
+install.packages("gridExtra")
+install.packages("RColorBrewer")
+install.packages("googleVis")
+install.packages("knitr")
+install.packages("tidyr")
+install.packages("car")
+install.packages("pander")
+
+
